30ee844cb24b4bd0eee9ad212c3502584c2f307d
freddii92 <arodriguez242@miners.utep.edu>
2020-09-07 19:30:03 -0600

    added print_arrow() to draw.h, draw_shapes.c and uimain.c

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,22 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+//Prints an arrow
+void print_arrow(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol + 3; col++) putchar(' ');
+    for (       ; col < endCol + 3;  col++) putchar('*');
+    putchar('\n');
+  }
+}
